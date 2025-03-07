bde96db3ed8eb552ee9e8aa0e0738e903ba251ea
cordelia-sixth <80014084+cordelia-sixth@users.noreply.github.com>
2025-03-07 17:08:47 +0900

    delete _root

--- src/pages/_layout.tsx
+++ src/pages/_layout.tsx
@@ -5,11 +5,26 @@ import '../styles.css';
 import { Header } from '../components/header';
 import { Footer } from '../components/footer';
 
+const code = `
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'G-35FY2D61LB');
+`;
+
 type RootLayoutProps = { children: ReactNode };
 
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
-    <div className="overflow-hidden font-['Nunito']">
+    <div className="overflow-hidden font-['Nunito'] has-[#sp-menu[data-expanded='true']]:overflow-hidden">
+      <script>{code}</script>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-35FY2D61LB"
+      ></script>
       <Header />
       <main className="m-6 flex justify-center *:min-h-64 *:min-w-64 lg:my-0 lg:min-h-svh lg:pb-20">
         {children}
